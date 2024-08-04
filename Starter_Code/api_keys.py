586584d7f87eceb78421b4276f90815b4ffb1b05
AbigailSerpa <abigailserpa@gmail.com>
2024-08-04 13:00:07 -0500

    updating .gitignore to modify the api_keys.py file to be untracked

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "27737a3648289ef83c9dbef02b0d959e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5400cd7688b7410f9bdd27833b9bca39"
